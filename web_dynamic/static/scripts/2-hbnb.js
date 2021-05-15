f44c4402b3f012c962e5b770aca01fbcf2794207
Kellie Mogg <kelliedmogg@gmail.com>
2021-05-15 04:15:58 +0000

    task 3 in progress

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  let amenList = [];
+  let value = '';
+  let amenString = "";
+  $('div.amenities ul li input').change(function () {
+    if ($(this).is(':checked')) {
+      value = $(this).attr('data-name');
+      amenList.push(value);
+    } else if (!$(this).is(':checked')) {
+      del_value = $(this).attr('data-name');
+      let index = amenList.indexOf(del_value);
+      if (index !== -1) {
+        amenList.splice(index, 1); }
+    }
+    if (amenList.length === 0) {
+      $('#checked_list').text('&nbsp;');
+    } else { amenString = amenList.join(', ');
+        $('#checked_list').text(amenString); }
+  });
+});
