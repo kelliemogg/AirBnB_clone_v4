c118af6104e869a29b660d7697c317723d50aaec
Kellie Mogg <kelliedmogg@gmail.com>
2021-05-17 00:06:08 +0000

    added ajax template

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
 });
 
 // this is our ajax function for live updates
-$(function () {
 
   $.ajax({
     type: 'POST',
@@ -20,11 +19,28 @@ $(function () {
 // loops through all places returned
 //this line grabs a place and then functions like it normally would
         const place = data[i];
-        //ajax html format goes here
-      })
-    }
-  })
-})
+              $.each(data, function () {
+          place = ['<div class="title_box">',
+          '<h2>' + data[i].name + '</h2>',
+          '<div class="price_by_night">' + data[i].price_by_night + '</div>',
+          '</div>',
+        '<div class="information">',
+          '<div class="max_guest">' + data[i].max_guest + '</div>',
+                '<div class="number_rooms">' + data[i].number_rooms + '</div>',
+              '<div class="number_bathrooms">' + data[i].number_bathrooms + '</div>',
+        '</div>',
+        '<div class="user">',
+                '<b>Owner:</b>' + data[i].first_name + ' ' + data[i].last_name,
+              '</div>',
+              '<div class="description">',
+          ' + data[i].description,',
+              '</div>'].join('\n');
+        $('sections.places').append(place);
+        i++;
+    });
+  }
+}
+});
 
 // this displays only selected amenities
   let amenList = [];
